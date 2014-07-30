573c0083c3729d05f7b7f2353547b0171931d31a
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2014-07-29 23:32:57 -0400

    Redo

--- lib/assets.php
+++ lib/assets.php
@@ -14,30 +14,29 @@
  * - An ID has been defined in config.php
  * - You're not logged in as an administrator
  */
-function roots_scripts() {
-  /**
-   * The build task in Grunt renames production assets with a hash
-   * Read the asset names from assets-manifest.json
-   */
-  if (WP_ENV === 'development') {
-    $assets = array(
-      'css'       => '/assets/css/main.css',
-      'js'        => '/assets/js/scripts.js',
-      'modernizr' => '/assets/vendor/modernizr/modernizr.js',
-      'jquery'    => '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js'
-    );
-  } else {
-    $get_assets = file_get_contents(get_template_directory() . '/assets/manifest.json');
-    $assets     = json_decode($get_assets, true);
-    $assets     = array(
-      'css'       => '/assets/css/main.min.css?' . $assets['assets/css/main.min.css']['hash'],
-      'js'        => '/assets/js/scripts.min.js?' . $assets['assets/js/scripts.min.js']['hash'],
-      'modernizr' => '/assets/js/vendor/modernizr.min.js',
-      'jquery'    => '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'
-    );
-  }
 
-  wp_enqueue_style('roots_css', get_template_directory_uri() . $assets['css'], false, null);
+function roots_load_manifest() {
+  $manifest = file_get_contents(get_template_directory() . '/assets/manifest.json');
+  return json_decode($manifest, true);
+}
+
+function roots_manifest($path) {
+  global $manifest;
+
+  if (WP_ENV == 'development') {
+    return $path;
+  } else {
+    $manifest = isset($manifest) ? $manifest : roots_load_manifest();
+    return $manifest[$path];
+  }
+}
+
+function roots_asset_path($path) {
+  return get_template_directory_uri() . '/assets/' . $path;
+}
+
+function roots_scripts() {
+  wp_enqueue_style('roots_css', $roots_asset_path('css/main.css'), array(), $roots_manifest('css/main.css'));
 
   /**
    * jQuery is loaded using the same method from HTML5 Boilerplate:
@@ -46,7 +45,13 @@ function roots_scripts() {
    */
   if (!is_admin() && current_theme_supports('jquery-cdn')) {
     wp_deregister_script('jquery');
-    wp_register_script('jquery', $assets['jquery'], array(), null, false);
+
+    if (WP_ENV == 'development') {
+      wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js', array(), null, false);
+    } else {
+      wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', array(), null, false);
+    }
+
     add_filter('script_loader_src', 'roots_jquery_local_fallback', 10, 2);
   }
 
@@ -54,9 +59,9 @@ function roots_scripts() {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_enqueue_script('modernizr', get_template_directory_uri() . $assets['modernizr'], array(), null, false);
+  wp_enqueue_script('modernizr', $roots_asset_path('vendor/modernizr/modernizr.js'), array(), $roots_manifest('vendor/modernizr/modernizr.js'), false);
   wp_enqueue_script('jquery');
-  wp_enqueue_script('roots_js', get_template_directory_uri() . $assets['js'], array(), null, true);
+  wp_enqueue_script('roots_js', $roots_asset_path('js/scripts.min.js'), array(), $roots_manifest('js/scripts.min.js'), true);
 }
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
 
@@ -65,7 +70,13 @@ function roots_jquery_local_fallback($src, $handle = null) {
   static $add_jquery_fallback = false;
 
   if ($add_jquery_fallback) {
-    echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/vendor/jquery/dist/jquery.min.js?1.11.1"><\/script>\')</script>' . "\n";
+    if (WP_ENV == 'development') {
+      $jquery = $roots_asset_path('vendor/jquery/dist/jquery.js?1.11.1');
+    } else {
+      $jquery = $roots_asset_path('vendor/jquery/dist/jquery.min.js?1.11.1');
+    }
+
+    echo '<script>window.jQuery || document.write(\'<script src="' . $jquery . '"><\/script>\')</script>' . "\n";
     $add_jquery_fallback = false;
   }
 
