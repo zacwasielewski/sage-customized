f3668a00d9942c91dee431947cce32f9e646b1bd
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2014-08-03 17:10:14 -0400

    Use wrapper functions

--- lib/assets.php
+++ lib/assets.php
@@ -6,14 +6,16 @@
  * 1. /theme/assets/css/main.css
  *
  * Enqueue scripts in the following order:
- * 1. jquery-1.11.1.min.js via Google CDN
- * 2. /theme/assets/js/vendor/modernizr.min.js
+ * 1. jquery-1.11.1.js via Google CDN
+ * 2. /theme/assets/js/vendor/modernizr.js
  * 3. /theme/assets/js/scripts.js (in footer)
  *
  * Google Analytics is loaded after enqueued scripts if:
  * - An ID has been defined in config.php
  * - You're not logged in as an administrator
  */
+
+$jquery_version = '1.11.1';
 
 function roots_load_manifest() {
   $manifest = file_get_contents(get_template_directory() . '/assets/manifest.json');
@@ -23,7 +25,7 @@ function roots_load_manifest() {
 function roots_manifest($path) {
   global $manifest;
 
-  if (WP_ENV != 'development') {
+  if (WP_ENV !== 'development') {
     $manifest = isset($manifest) ? $manifest : roots_load_manifest();
     return $manifest["assets/$path"]['hash'];
   }
@@ -33,8 +35,22 @@ function roots_asset_path($path) {
   return get_template_directory_uri() . '/assets/' . $path;
 }
 
+function roots_enqueue_script($handle, $src, $deps = array(), $in_footer = false) {
+  $src = roots_asset_path($src);
+  $ver = roots_manifest($src);
+
+  wp_enqueue_script($handle, $src, $deps, $ver, $in_footer);
+}
+
+function roots_enqueue_style($handle, $src, $deps = array(), $media = 'all') {
+  $src = roots_asset_path($src);
+  $ver = roots_manifest($src);
+
+  wp_enqueue_style($handle, $src, $deps, $ver, $media);
+}
+
 function roots_scripts() {
-  wp_enqueue_style('roots_css', roots_asset_path('css/main.css'), array(), roots_manifest('css/main.css'));
+  roots_enqueue_style('roots_css', 'css/main.css');
 
   /**
    * jQuery is loaded using the same method from HTML5 Boilerplate:
@@ -45,9 +61,9 @@ function roots_scripts() {
     wp_deregister_script('jquery');
 
     if (WP_ENV == 'development') {
-      wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js', array(), null, false);
+      wp_register_script('jquery', "//ajax.googleapis.com/ajax/libs/jquery/$jquery_version/jquery.js", array(), null);
     } else {
-      wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', array(), null, false);
+      wp_register_script('jquery', "//ajax.googleapis.com/ajax/libs/jquery/$jquery_version/jquery.min.js", array(), null);
     }
 
     add_filter('script_loader_src', 'roots_jquery_local_fallback', 10, 2);
@@ -57,9 +73,9 @@ function roots_scripts() {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_enqueue_script('modernizr', roots_asset_path('vendor/modernizr/modernizr.js'), array(), roots_manifest('vendor/modernizr/modernizr.js'), false);
+  roots_enqueue_script('modernizr', 'vendor/modernizr/modernizr.js');
   wp_enqueue_script('jquery');
-  wp_enqueue_script('roots_js', roots_asset_path('js/scripts.js'), array(), roots_manifest('js/scripts.js'), true);
+  roots_enqueue_script('roots_js', 'js/scripts.js', array(), true);
 }
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
 
@@ -69,9 +85,9 @@ function roots_jquery_local_fallback($src, $handle = null) {
 
   if ($add_jquery_fallback) {
     if (WP_ENV == 'development') {
-      $jquery = roots_asset_path('vendor/jquery/dist/jquery.js?1.11.1');
+      $jquery = roots_asset_path("vendor/jquery/dist/jquery.js?$jquery_version");
     } else {
-      $jquery = roots_asset_path('vendor/jquery/dist/jquery.min.js?1.11.1');
+      $jquery = roots_asset_path("vendor/jquery/dist/jquery.min.js?$jquery_version");
     }
 
     echo '<script>window.jQuery || document.write(\'<script src="' . $jquery . '"><\/script>\')</script>' . "\n";
