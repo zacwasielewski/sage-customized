5379961d0fc1335cd75c2014b86e81cecb21e8cd
Zac Wasielewski <zac@wasielewski.org>
2015-09-10 13:41:57 -0400

    Integrate tgm-plugin-activation library with theme

--- functions.php
+++ functions.php
@@ -18,6 +18,7 @@ $sage_includes = [
   'lib/assets.php',                // Scripts and stylesheets
   'lib/titles.php',                // Page titles
   'lib/extras.php',                // Custom functions
+  'lib/custom-plugin-activation.php',
 ];
 
 foreach ($sage_includes as $file) {
